82acb3e42086dfab4ad40928874f16d3f8b87ca2
584859 <lefis@naver.com>
2022-03-30 22:13:05 +0900

    [조성훈] 3/30 포트폴리오 초안 올리기

--- src/main/java/com/honftel/project/config/WebMVCConfig.java
+++ src/main/java/com/honftel/project/config/WebMVCConfig.java
@@ -1,6 +1,7 @@
 package com.honftel.project.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
